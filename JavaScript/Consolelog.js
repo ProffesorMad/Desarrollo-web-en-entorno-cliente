984d40c4b3b22b8b8eb8a5468637337d40c4d315
Pablico <99720054+ProffesorMad@users.noreply.github.com>
2025-09-30 12:10:46 +0200

    2.12 Ejercicios Js

---
+++ JavaScript/Consolelog.js
@@ -0,0 +1,20 @@
+console.log('A la gente de Zaragoza se les llama "Maños"');
+
+console.log('Nacimos en los 70’s');
+
+console.log('A la gente de Zaragoza se les llama "Maños" Nacimos en los 70’s');
+
+alert('"Maños" de los 70’s');
+
+let b = "cadena";
+console.log(b + " " + b);
+
+var txt = "SUPERCALIFRAGILISTICOESPIALIDOSO";
+alert(txt.length);
+
+document.body.style.backgroundColor = "red";
+
+alert("El número \"\u03c0\" tiene un valor de " + Math.PI);
+
+alert("Buenos dias,\n\tque tal estais?:\nYo genial: \'\nMe alegro: \"");
+
