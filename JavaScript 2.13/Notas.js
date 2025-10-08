15640b2f1b8cd27fe5c814258b5f91310c9bf8f3
Pablico <99720054+ProffesorMad@users.noreply.github.com>
2025-10-08 12:43:52 +0200

    2.13 Ejercicios Js

---
+++ JavaScript 2.13/Notas.js
@@ -0,0 +1,7 @@
+//Crea una aplicación Web que mediante JavaScript calcule la nota de la primera
+//evaluación de la asignatura de Desarrollo Web Entorno Cliente.
+
+
+
+//Realiza una aplicación Web, que mediante JavaScript verifique el DNI. Se
+//solicitará dos entradas al usuario.
